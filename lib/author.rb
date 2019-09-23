3af712b07226c8ee355447566d825c06a2696b97
Alfredo Diaz <mail4alfredo@gmail.com>
2019-09-23 11:50:44 +0000

    Automatically backed up by Learn

---
+++ lib/author.rb
@@ -0,0 +1,46 @@
+class Author
+   attr_accessor :name
+   
+   @@all = []
+   
+   def initialized(name)
+     @name = name
+     @@all << self
+   end 
+   
+   def self.all
+    @@all
+   end
+  
+  def self.all
+    @@all
+  end
+
+  def add_post(post)
+    post.author = self
+  end
+
+  def add_post_by_title(title)
+    post = Post.new(title)
+    post.author = self
+  end
+
+  def posts
+    Post.all.select {|post| post.author == self}
+  end
+
+  #   def author_name
+  #   if self.author!=nil
+  #     self.author.name
+  #       elsif self.author=nil
+  #         nil
+  # end
+  #   end
+
+  def self.post_count
+    Post.all.count
+  end
+end
+  
+  
+ 
